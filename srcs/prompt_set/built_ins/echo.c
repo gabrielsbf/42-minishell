549b5e99f2758c306a72617802e969896003eca5
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-06-12 01:25:52 -0300

    echo function and binary completed

---
+++ srcs/prompt_set/built_ins/echo.c
@@ -0,0 +1,31 @@
+#include "../../../includes/minishell.h"
+
+void    echo(char *argv[])
+{
+    int i;
+    int trailing;
+    
+    // printf("in echo, arg 0 is : %s\n", argv[0]);
+    trailing = 0;
+    i = 1;
+    if (ft_strncmp(argv[1], "-n", 3) == 0)
+        i++;
+    else
+        trailing = 1;
+    while (argv[i] != NULL)
+    {
+        printf("%s", argv[i]);
+        if (argv[i + 1] != NULL)
+            printf(" ");
+        i++;
+    }
+    if (trailing == 1)
+        printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    // printf("runing echo in main\n");
+    echo(argv);
+}
