f9b94d524821b5af16c5bb19f83843726eaaad7d
gabrielsbf <gabrielsbfernandes@gmail.com>
2024-06-17 21:15:50 -0300

    includes for server initialization and reestruct of pipe

--- includes/server.h
+++ includes/server.h
@@ -1,4 +1,11 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
+char    *prompt_prefix(void);
+void	free_prompt_el(char *prefix, char *line_read);
+void	server_loop(t_prompt **prompt_st);
+
 #endif
