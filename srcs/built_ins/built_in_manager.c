d553d7e41d4f723ce8f6ae1727884846f155d6de
Bruno Kwamme <ambsouza@gmail.com>
2024-07-17 17:03:04 -0300

    built in updated to work with new parser

--- srcs/built_ins/built_in_manager.c
+++ srcs/built_ins/built_in_manager.c
@@ -1,19 +1,13 @@
 #include "../../includes/minishell.h"
 
-void	function_listener(char **splitted_instructions)
+void	function_listener(t_parse **parser)
 {
-	int	i;
-
-	i = 0;
-	while (splitted_instructions[i] != NULL)
+	if (ft_strncmp((*parser)->main_command, "echo", 4) == 0)
 	{
-		if (ft_strncmp(splitted_instructions[i], "echo", 4) == 0)
-			ft_echo (splitted_instructions);
-		else if (ft_strncmp(splitted_instructions[i], "fork", 4) == 0)
-			ft_fork ();
-		else if (ft_strncmp(splitted_instructions[i], "exit", 4) == 0)
-			exit(0);
-		i++;
+		ft_echo((*parser)->arguments);
+		//o parse->flag está incompleto. mas o builtin e o parser já estão conectados
 	}
+	else if(ft_strncmp((*parser)->main_command, "exit", 4) == 0)
+		exit(0);
 }
 
--- srcs/built_ins/echo.c
+++ srcs/built_ins/echo.c
@@ -7,8 +7,8 @@ void	ft_echo(char **splitted_instructions)
 
 	// printf("in echo, arg 0 is : %s\n", splitted_instructions[0]);
 	trailing = 0;
-	i = 1;
-	if (ft_strncmp(splitted_instructions[1], "-n", 3) == 0)
+	i = 0;
+	if (ft_strncmp(splitted_instructions[0], "-n", 3) == 0)
 		i++;
 	else
 		trailing = 1;
