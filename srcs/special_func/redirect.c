49752c941fb0f75fce09f619c55cca6aaf4a1810
Bruno <ambsouza@gmail.com>
2024-10-16 14:20:15 -0300

    hotfix-LIMPEZA DA CASA

--- srcs/special_func/redirect.c
+++ srcs/special_func/redirect.c
@@ -3,13 +3,11 @@
 void	redirect(t_parse **parser)
 {
 	t_parse *temp;
-	int		fd;
 
 	temp = (*parser)->next;
-	fd = 0;
 	if (temp->main_command == NULL)
 		return ;
-	fd = open(temp->main_command, O_WRONLY | O_CREAT, 0777);
-	dup2 (fd, STDOUT_FILENO);
-	close(fd);
+	temp->fd_out = open(temp->main_command, O_WRONLY | O_CREAT, 0777);
+	/* dup2 (temp->fd_out, STDOUT_FILENO); */
+	close(temp->fd_out);
 }
