bfd03b7cd60520c788263b531fad834af6f34526
Bruno <ambsouza@gmail.com>
2024-10-24 14:47:23 -0300

    updating free management

--- srcs/built_ins/exit.c
+++ srcs/built_ins/exit.c
@@ -1,86 +1,25 @@
  #include "../../includes/minishell.h"
 
-void	free_env(t_env **env)
-{
-	t_env	*temp;
-
-	if (!env)
-		return ;
-	while ((*env))
-	{
-		temp = (*env);
-		free((*env)->value);
-		free((*env)->name);
-		(*env)->value = NULL;
-		(*env)->name = NULL;
-		(*env)->head = NULL;
-		(*env) = (*env)->next;
-		free(temp);
-	}
-}
-
-void	free_args(char **args)
-{
-	int	i;
-
-	if (!args)
-		return ;
-	i = 0;
-	while (args[i] != NULL)
-	{
-		free(args[i]);
-		i++;
-	}
-	free(args);
-}
-
-void	free_parser(t_parse **parser)
-{
-	t_parse	*temp;
-
-	if (!(*parser))
-		return ;
-	while ((*parser))
-	{
-		temp = (*parser);
-		free_args((*parser)->arguments);
-		free_args((*parser)->env_path);
-		free_args((*parser)->exec_txt);
-		free_args((*parser)->flags);
-		free((*parser)->command_text);
-		free((*parser)->entire_text);
-		free((*parser)->main_command);
-		free((*parser)->special_char);
-		(*parser)->head = NULL;
-		(*parser) = (*parser)->next;
-		free(temp);
-	}
-}
-
-
-void	free_all(t_env **env, t_parse **parser)
+void	sigquit_exit(t_env **env, t_parse **parser)
 {
 	free_env(env);
 	free_parser(parser);
-}
-
-void	sigquit_exit(t_env **env, t_parse **parser)
-{
-	free_all(env, parser);
 	ft_putendl_fd("exit", 1);
 	exit(EXIT_SUCCESS);
 }
 
 void	non_numeric_exit(t_env **env, t_parse **parser)
 {
-	free_all(env, parser);
+	free_env(env);
+	free_parser(parser);
 	ft_putstr_fd("exit\nnumeric argument required\n", STDOUT_FILENO);
 	exit(2);
 }
 
 void	many_args_exit(t_env **env, t_parse **parser)
 {
-	free_all(env, parser);
+	free_env(env);
+	free_parser(parser);
 	ft_putstr_fd("exit\ntoo many arguments\n", STDOUT_FILENO);
 	exit(1);
 }
@@ -105,7 +44,8 @@ void	ft_exit(t_parse **parser, t_env **env)
 		}
 		temp = ft_atoi((*parser)->arguments[1]);
 	}
-	free_all(env, parser);
+	free_env(env);
+	free_parser(parser);
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (temp)
 		exit(temp);
--- srcs/prompt_set/server_init.c
+++ srcs/prompt_set/server_init.c
@@ -16,25 +16,36 @@ char	*prompt_prefix(void)
 	prompt_prefix = ft_strjoin(temp, "$ ");
 	return (prompt_prefix);
 }
-
-/*Still need to do
-free the parser struct being used.*/
-void	free_parser_st(t_parse **parser)
+/*
+void	free_onloop(t_parse **parser, char *prefix_elem, char *line_read)
 {
-	free((*parser));
-	return ;
-}
-void	free_onloop(t_parse **parser, char *prefix_elem, char *line_read, int free_parse)
-{
-	if (parser != NULL && free_parse == 1)
-	{
-		free_parser_st(parser);
-	}
+	if (parser != NULL)
+		free_parser(parser);
 	if (prefix_elem != NULL)
 		free(prefix_elem);
 	if (line_read != NULL)
 		free(line_read);
+} */
 
+int	data_validation(char *line_read, char *prefix_element, t_parse **parser, t_env **env)
+{
+	if (!line_read)
+	{
+		free(prefix_element);
+		free(line_read);
+		sigquit_exit(env, parser);
+	}
+	if (ft_strcmp(line_read, "") != 0)
+		add_history(line_read);
+	*parser = main_line_process(line_read, env);
+	if ((*parser) == NULL)
+	{
+		free(line_read);
+		free(prefix_element);
+		free_parser(parser);
+		return (1);
+	}
+	return (0);
 }
 
 void	server_loop(t_env **env, char **envp)
@@ -46,22 +57,29 @@ void	server_loop(t_env **env, char **envp)
 	while
 	(1)
 	{
+		rl_replace_line("", 0);
 		prefix_element = prompt_prefix();
 		line_read = readline(prefix_element);
+		/*if (!line_read)
+		{
+			free(line_read);
+			sigquit_exit(env, &parser);
+		}
 		if (ft_strcmp(line_read, "") != 0)
 			add_history(line_read);
 		parser = main_line_process(line_read, env);
 		if (parser == NULL)
 		{
-			free_onloop(&parser, prefix_element, line_read, 0);
+			free_onloop(&parser, prefix_element, line_read);
 			continue;
 			//include a free process here
-		}
-		free(prefix_element);
+		}*/
+		if (data_validation(line_read, prefix_element, &parser, env) == 1)
+			continue ;
 		env_expansion(&parser, env);
 		sp_char_exec(&parser, env, envp);
 		print_parser_struct(parser);
 		function_listener(&parser, env, envp);
-		free(parser);
+		free_onloop(&parser, prefix_element, line_read);
 	}
 }
